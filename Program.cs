e5934bd7c79eb288b9bccd1a0cc0288e22b58a08
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-12-16 16:30:55 +0300

    Finished project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class GuessingGame
+{
+    public static void Main(string[] args)
+    {
+        int max = 100;
+        int random = new Random().Next(1, max);
+        int guess = 0;
+
+        Console.WriteLine("Welcome to my Guessing Game!");
+        Console.WriteLine("Guess a number between 1 and " + max);
+
+        // check if the first guess is not a number
+        if (!int.TryParse(Console.ReadLine(), out guess))
+        {
+            Console.WriteLine("That's not a number!");
+            return;
+        }
+
+        while (guess != random)
+        {
+            if (guess < random)
+            {
+                Console.WriteLine("Too low, try again!");
+            }
+            else if (guess > random)
+            {
+                Console.WriteLine("Too high, try again!");
+            }
+
+            // read the next guess at the end of the loop
+            if (!int.TryParse(Console.ReadLine(), out guess))
+            {
+                Console.WriteLine("That's not a number!");
+                return;
+            }
+        }
+
+        // print the success message after the loop
+        Console.WriteLine("You guessed it!");
+    }
+}
